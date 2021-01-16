c1d87fcbba96b50e7d8eff8ae6e69bc1f2fb6ee2
leviafan14 <39337816+leviafan14@users.noreply.github.com>
2021-01-16 12:55:02 +0500

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -2,8 +2,8 @@
 import Inettel_insta_bot
 from Inettel_insta_bot import InettelInstaBot
 #Данные для авторизации
-username = 'inettel.provider'
-password = '8845svmIns'
+username = ''
+password = ''
 
 #Запуск бота
 if __name__ == '__main__':
