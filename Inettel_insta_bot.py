488da16616e8395e7f05fcc619ac1147f8a808c5
leviafan14 <14leviafan88@gmail.com>
2021-03-06 23:27:03 +0500

    edit 06_03

--- Inettel_insta_bot.py
+++ Inettel_insta_bot.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 import random
 
-#Описание класса бота
+# Описание класса бота
 class InettelInstaBot():
     def __init__(self,username,password):
         self.username=username
@@ -16,27 +16,27 @@ class InettelInstaBot():
         self.browser.close()
         self.browser.quit()
 
-    #Авторизация
+    # Авторизация
     def login(self):
         try:
             browser = self.browser
             browser.get('https://www.instagram.com')
             time.sleep(random.randrange(3, 5))
             
-            #Ввод логина
+            # Ввод логина
             username_input = browser.find_element_by_name('username')
             username_input.clear()
             username_input.send_keys(self.username)
             time.sleep(2)
             
-            #Ввод пароля
+            # Ввод пароля
             password_input = browser.find_element_by_name('password')
             password_input.clear()
             password_input.send_keys(self.password)
             password_input.send_keys(Keys.ENTER)
             time.sleep(10)
             
-            #Закрытие всплывающих окон после авторизации
+            # Закрытие всплывающих окон после авторизации
             try:
                 element = browser.find_element_by_class_name('sqdOP')
                 element.click()
@@ -53,8 +53,9 @@ class InettelInstaBot():
             print(main_auth_except)
 
         
-    #Просмотр сторис
+    # Просмотр сторис
     def storis(self):
+        print('Смотрю сторис пользователей')
         fragment_storis = []
         time.sleep(5)
         try:
@@ -72,9 +73,9 @@ class InettelInstaBot():
                     print('error progress line', except_progress)
                     break
                 try:
-                    time.sleep(2)
+                    time.sleep(3)
                     next_storis_button = browser.find_element_by_class_name('coreSpriteRightChevron')
-                    time.sleep(1)
+                    time.sleep(2)
                     next_storis_button.click()
                 except Exception as ex:
                     print('coreSpriteRightChevron ',ex)        
@@ -86,7 +87,7 @@ class InettelInstaBot():
         return 1
 
 
-    #Ставим лайк на запись 
+    # Ставим лайк на запись 
     def get_like(self):
         #Получаем ссылки на указанное количество постов
         browser = self.browser
@@ -95,28 +96,29 @@ class InettelInstaBot():
         count = 1
         body = browser.find_element_by_tag_name('body')
         
-        #Здесь указано количество ссылок на посты, которые н нужно посмотреть
+        # Здесь указано количество ссылок на посты, которые н нужно посмотреть
         count_posts=random.randrange(12, 13)
         print('Будет просмотрено:', count_posts)
         while len(urls_set)<=count_posts:
             
-            #эмитация нажатия кнопки Page down для прокрутки страницы
+            # Эмитация нажатия кнопки Page down для прокрутки страницы
             body.send_keys(Keys.PAGE_DOWN)
             time.sleep(3)
             href = browser.find_element_by_class_name('c-Yi7')
             try:
-                #Проверяем является ли полученная ссылка записью пользователя,
-                #Если является, то добавляем её во множество
+                # Проверяем является ли полученная ссылка записью пользователя,
+                # Если является, то добавляем её во множество
                 if "/p/" in href.get_attribute('href'):
                     urls_set.add(href.get_attribute('href'))
                     count += 1
+                    print(href.get_attribute('href'))
                     print('iteracion: ',count,' len :',len(urls_set))
             except Exception as e:
                 print('error ', e)
-            time.sleep(3)
+            time.sleep(4)
 
             
-        #Проверяем полученные ссылки на посты, если лайка нет, то ставим его
+        # Проверяем полученные ссылки на посты, если лайка нет, то ставим его
         print('len:',len(urls_set))
         for url in urls_set:
             try:
@@ -127,14 +129,14 @@ class InettelInstaBot():
                 for s in svg:
                     fill = s.get_attribute('fill')
                     label = s.get_attribute('aria-label')
-                    #Проверка, если лайка нет, то бот его ставит
+                    # Проверка, если лайка нет, то бот его ставит
                     if fill=='#262626' and label=='Нравится':
                          print('Ставлю лайк')
                          time.sleep(3)
                          like_button =browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
                          break
                          time.sleep(3)
-                    #Если лайк есть, то бот выходит из цикла svg
+                    # Если лайк есть, то бот выходит из цикла svg
                     elif fill=='#ed4956' and label=='Не нравится':
                         print('Уже есть лайк')
                         break
@@ -145,19 +147,19 @@ class InettelInstaBot():
                 print(ex)
 
     
-    #Подписываемся на пользователей
+    # Подписываемся на пользователей
     def subscribe(self):
         browser = self.browser
         browser.get('https://www.instagram.com/explore/people/suggested/')
         users_links = set()
-        #Отсчет для подписки на пользователей
+        # Отсчет для подписки на пользователей
         count = 1
-        #Рандомное количество пользователей на которых подпишется бот
+        # Рандомное количество пользователей на которых подпишется бот
         count_new_subscribes=random.randrange(3, 5) 
         time.sleep(5)
         print('Количество новых подписок:', count_new_subscribes)
         
-        #Получаем ссылки на аккаунты
+        # Получаем ссылки на аккаунты
         users = browser.find_elements_by_class_name('FPmhX.notranslate.MBL3Z')
         for i in users:
             if count <= count_new_subscribes:
@@ -168,8 +170,8 @@ class InettelInstaBot():
         for user_url in users_links:
             browser.get(user_url)
             time.sleep(3)
-            #Если профиль закрытый - то его пропускаем и
-            #переходим к следующему пользователю
+            # Если профиль закрытый - то его пропускаем и
+            # переходим к следующему пользователю
             try:
                 browser.find_element_by_class_name('rkEop')
                 print('Закрытый аккаунт, его пропускаем')
@@ -177,7 +179,7 @@ class InettelInstaBot():
             except Exception:
                 print('Открытый профиль')
         
-            #Получаем кнопку и проверяем, является ли она кнопкой "Подписаться"
+            # Получаем кнопку и проверяем, является ли она кнопкой "Подписаться"
             try:
                 subscribe_button = browser.find_element_by_class_name('_5f5mN.jIbKX._6VtSN.yZn4P')
                 text = subscribe_button.text #Получаем текст кнопки
@@ -188,10 +190,10 @@ class InettelInstaBot():
                 else:
                     continue
             except Exception as e:
-            #Если кнопка не найдена по классу, то возбуждаем исключение
+            # Если кнопка не найдена по классу, то возбуждаем исключение
                 print('Ошибка. Кнопка Подписаться не найдена ',e)
             
-            #Получаем кнопку и проверяем, является ли она кнопкой "Подписаться в ответ"
+            # Получаем кнопку и проверяем, является ли она кнопкой "Подписаться в ответ"
             try:
                 subscribe_button=browser.find_element_by_class_name('_5f5mN.jIbKX._6VtSN.yZn4P')
                 text=subscribe_button.text #Получаем текст кнопки
@@ -203,7 +205,7 @@ class InettelInstaBot():
             except Exception as e:
                 print('Ошибка. Кнопка Подписаться в ответ не найдена ',e)
                 
-            #выжидаем случайный период времени перед след. аккаунтом
+            # выжидаем случайный период времени перед след. аккаунтом
             time.sleep(random.randrange(3, 5))
         print (users_links)
         browser.get('https://www.instagram.com/explore/people/suggested/')
